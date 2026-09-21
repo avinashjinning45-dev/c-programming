a03c35d92aea99448a9b411bfb55ecd369ff79b4
avinashjinning45-dev <avinashjinning45@gmail.com>
2026-09-21 19:20:38 +0530

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks;
+
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 35)
+    {
+        printf("Pass");
+    }
+    else
+    {
+        printf("Fail");
+    }
+
+    return 0;
+}
